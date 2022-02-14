bff3c83618ff463f0a5323f56f9ce9eddb19ff19
Adriana Ito <ito.dri@gmail.com>
2022-02-14 19:49:51 +0900

    changes isFinalized variable to isLoading

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React from "react";
 import PropTypes from "prop-types";
 
@@ -5,16 +6,26 @@ import { Hero } from "components/ui/common";
 import { List } from "components/ui/course";
 import { BaseLayout } from "components/ui/layout";
 import getAllProducts from "content/products/fetcher";
+import { useWeb3 } from "components/providers";
 
 const propTypes = {
   products: PropTypes.arrayOf(PropTypes.object),
 };
-const Home = ({ products }) => (
-  <div>
-    <Hero />
-    <List products={products} />
-  </div>
-);
+const Home = ({ products }) => {
+  const { web3, isLoading } = useWeb3();
+  console.log(web3);
+  return (
+    <div>
+      {isLoading
+        ? "Is loading web3..."
+        : web3
+        ? "Web3 is ready"
+        : "Please install MetaMask"}
+      <Hero />
+      <List products={products} />
+    </div>
+  );
+};
 
 Home.Layout = BaseLayout;
 
