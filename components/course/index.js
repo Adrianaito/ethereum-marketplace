948615aaf3a3ca8d933788cc6a1dbdfd7880298f
Adriana Ito <ito.dri@gmail.com>
2022-02-14 15:18:36 +0900

    exports components

---
+++ components/course/index.js
@@ -0,0 +1,4 @@
+export { default as List } from "./List";
+export { default as Hero } from "./Hero";
+export { default as Keypoints } from "./Keypoints";
+export { default as Topics } from "./Topics";
