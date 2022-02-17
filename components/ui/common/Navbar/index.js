975c211929156ba2f2cea54d8c6a9e720af28964
Adriana Ito <ito.dri@gmail.com>
2022-02-17 15:56:13 +0900

    adds route to download metamask button

--- components/ui/common/Navbar/index.js
+++ components/ui/common/Navbar/index.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import { useWeb3 } from "components/providers";
 import { Button } from "components/ui/common";
 
 const Navbar = () => {
+  const router = useRouter();
   const { connect, isLoading, isWeb3Loaded } = useWeb3();
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div className="flex">
               <Link href="/">
                 <p className="font-medium mr-8 text-gray-500 hover:text-gray-900 cursor-pointer">
@@ -35,29 +37,14 @@ const Navbar = () => {
                 </p>
               </Link>
               {isLoading ? (
-                <Button
-                  onClick={() => connect()}
-                  onKeyUp={connect}
-                  role="button"
-                  tabIndex={0}
-                >
+                <Button onClick={connect} disabled>
                   Loading...
                 </Button>
               ) : isWeb3Loaded ? (
-                <Button
-                  onClick={() => connect()}
-                  onKeyUp={connect}
-                  role="button"
-                  tabIndex={0}
-                >
-                  Connect
-                </Button>
+                <Button onClick={connect}>Connect</Button>
               ) : (
                 <Button
-                  onClick={() => connect()}
-                  onKeyUp={connect}
-                  role="button"
-                  tabIndex={0}
+                  onClick={() => router.push("https://metamask.io/download/")}
                 >
                   Install Metamask
                 </Button>
